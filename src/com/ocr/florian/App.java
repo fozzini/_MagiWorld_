c29d6af8ca1aa6c29105a045c01fbab244b7872f
fozzini <ffoezon@gmail.com>
2019-10-19 16:40:30 +0200

    allegement du code app à l'aide de boucles et tableau d'objets

--- src/com/ocr/florian/App.java
+++ src/com/ocr/florian/App.java
@@ -2,51 +2,78 @@ package com.ocr.florian;
 
 import java.util.Scanner;
 
+import static java.lang.System.out;
+
 public class App {
 
     static Scanner sc = new Scanner(System.in);
 
+//creation des attributs de class   ====================================================================================
+    private static int i;
     private static int nom;
     private static int niveau;
     private static int vie;
     private static int force;
-    private static int intelligence;
     private static int agilite;
-    
+    private static int intelligence ;
+
 
     public static void main(String[] args) {
 
-        //création du Joueur1===========================================================================================
-        System.out.println("Création du Personnage du Joueur1");
-        AbstractPersonnage.builder();
-        Guerrier player1 = new Guerrier(1,nom,niveau,vie,force,agilite,intelligence);
+//création des instances du Joueur1 et 2  ==============================================================================
 
-        //création du Joueur2===========================================================================================
-        System.out.println("Création du Personnage du Joueur2");
-        AbstractPersonnage.builder();
-        Guerrier player2 = new Guerrier(2,nom,niveau,vie,force,agilite,intelligence);
+        Guerrier[] player = new Guerrier[3];
 
+        for(i=1; i<3; i++){
 
+            System.out.println("Création du Personnage du Joueur "+ i);
+            builder();
+            player[i] = new Guerrier(i,nom,niveau,vie,force,agilite,intelligence);
+            System.out.println(player[i].talk());
+        }
 
+//reinitialisation de i et lancement du combat à l'aide d'une boucle while et for=======================================
 
-        //début du jeu==================================================================================================
+        i = 1;
+        while (player[i].getVie() > 0) {
 
-        while ((player1.getVie() >= 0) || player2.getVie() >= 0) {
-            //player1.attaque();
-            if (player2.getVie() <= 0) {
-                System.out.println("Joueur 2 à perdu !");
-                return;
+            for(i = 1; i < 3; i++){
+                out.println("Joueur"+i+ " " +player[i].getVie()+ " Vitalité Veuillez choisir votre action " +
+                        "(1 : Attaque basique, 2 : Attaque spéciale)");
+                int action = sc.nextInt();
+
+                if(action == 1){
+                    player[1].attaqueBasique();
+                }
+                else if (action == 2){
+                    player[1].attaqueSpeciale();
+                }
             }
-            //player2.attaque();
-
-            if (player1.getVie() >= 0) {
-                System.out.println("Joueur 1 à perdu !");
-                return;
-
-            }
+            i = 1;
+        }
+        if (player[i].getVie() < 0) {
+            System.out.println("Joueur"+i+" à perdu !");
+            return;
         }
     }
+//méthode d'aquisition des attributs de class  =========================================================================
+
+    public static void  builder(){
+
+        System.out.println("Veuillez choisir la classe de votre personnage (1 : Guerrier, 2 : Rôdeur, 3 : Mage)");
+        nom = sc.nextInt();
+        System.out.println("Niveau du personnage ?");
+        niveau = sc.nextInt();
+        vie = niveau * 5;
+        System.out.println("Force du personnage ?");
+        force = sc.nextInt();
+        System.out.println("Agilité du personnage ?");
+        agilite = sc.nextInt();
+        System.out.println("Intelligence du personnage  ?");
+        intelligence = sc.nextInt();
+    }
 }
+
 
 
 
--- src/com/ocr/florian/Guerrier.java
+++ src/com/ocr/florian/Guerrier.java
@@ -1,44 +1,54 @@
 package com.ocr.florian;
 
+import java.util.Scanner;
+
 import static java.lang.System.out;
 
 public class Guerrier {
 
-    private static int j;
-    private static int nom;
-    private static int niveau;
-    private static int vie;
-    private static int force;
-    private static int agilite;
-    private static int intelligence;
+    static Scanner sc = new Scanner(System.in);
+    //creation attribut d'instance======================================================================================
+    int i;
+    int nom;
+    int niveau;
+    int vie;
+    int force;
+    int agilite;
+    int intelligence;
+    //creation des getters==============================================================================================
 
-    public static int getVie() {
+    public int getVie() {
         return vie;
     }
 
-    Guerrier(int j, int nom, int niveau, int vie, int force, int agilite, int intelligence) {
-        this.j = j;
-        Guerrier.nom = nom;
-        Guerrier.niveau = niveau;
-        Guerrier.vie = vie;
-        Guerrier.force = force;
-        Guerrier.agilite = agilite;
-        Guerrier.intelligence = intelligence;
+    public int getI() {
+        return i;
+    }
+//creation du constructeur d'objet Guerrier=============================================================================
+
+    Guerrier(int i, int nom, int niveau, int vie, int force, int agilite, int intelligence) {
+        this.i = i;
+        this.nom = nom;
+        this.niveau = niveau;
+        this.vie = vie;
+        this.force = force;
+        this.agilite = agilite;
+        this.intelligence = intelligence;
     }
 
-    public static String talk () {
-        String s = "Woarg je suis le Guerrier Joueur"+j+" niveau " + niveau + " je possède " + vie + " de vitalité, " + force + " de force, " + agilite + " d'agilité et " + intelligence + " d'intelligence !";
+    public String talk () {
+        String s = "Woarg je suis le Guerrier Joueur"+i+" niveau " + niveau + " je possède " + vie + " de vitalité, " + force + " de force, " + agilite + " d'agilité et " + intelligence + " d'intelligence !";
         return s;
 
     }
 
-    static int attaqueBasique () {
+    int attaqueBasique () {
             out.println("Joueur1 utilise Coup d'Epée et inflige " + force + " dommages.");
             out.println("Joueur 2 perd " + force + " points de vie");
             return force;
 
     }
-    static int attaqueSpeciale () {
+    int attaqueSpeciale () {
         out.println("Joueur 1 utilise Coup de Rage et inflige " + force * 2 + " dommages.");
         out.println("Joueur 2 perd " + force * 2 + " points de vie");
         out.println("Joueur 1 perd " + force / 2 + " points de vie");
